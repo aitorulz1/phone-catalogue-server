169fda7b7e781d828a6f59f4373ac45d682662ee
Aitor <aitorulz1@gmail.com>
2020-11-12 16:43:02 +0100

    CORS setted

--- app.js
+++ app.js
@@ -10,6 +10,19 @@ require('./config/db');
 
 //CORS on ExpressJS | cross origin problems
 app.use(cors());
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+
+  app.options('*', (req, res) => {
+      res.header('Access-Control-Allow-Methods', 'GET, PATCH, PUT, POST, DELETE, OPTIONS');
+      res.send();
+  });
+});
+
+
 app.use(bodyParser.json());
 app.use(express.json());
 
